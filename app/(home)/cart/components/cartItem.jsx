83358b062cb910189e5585046e2e1160bb48663c
shadon016 <shadon016@gmail.com>
2025-05-07 19:03:48 +0600

    update cart item

--- app/(home)/cart/components/cartItem.jsx
+++ app/(home)/cart/components/cartItem.jsx
@@ -3,11 +3,30 @@ import React, { useContext } from "react";
 import { cartContext } from "../../../context/context.js";
 
 const CartItem = () => {
-  const { cart } = useContext(cartContext);
+  const { cart, setCart } = useContext(cartContext);
   const totalPrice = cart.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
   );
+
+  const handleDelete = (id) => {
+    const updatedCart = cart.filter((item) => item.id !== id);
+    setCart(updatedCart);
+  };
+  const handleIncrement = (id) => {
+    const updatedCart = cart.map((item) =>
+      item.id === id ? { ...item, quantity: item.quantity + 1 } : item
+    );
+    setCart(updatedCart);
+  };
+  const handleDecrement = (id) => {
+    const updatedCart = cart.map((item) =>
+      item.id === id && item.quantity > 1
+        ? { ...item, quantity: item.quantity - 1 }
+        : item
+    );
+    setCart(updatedCart);
+  };
   return (
     <div className="flex flex-col gap-4">
       {cart?.map((item) => (
@@ -21,8 +40,9 @@ const CartItem = () => {
             <p>Quantity: {item.quantity}</p>
           </div>
           <div className="flex gap-2">
-            
-
+            <button onClick={() => handleIncrement(item.id)}>+</button>
+            <button onClick={() => handleDecrement(item.id)}>-</button>
+            <button onClick={() => handleDelete(item.id)}>delete</button>
           </div>
         </div>
       ))}
