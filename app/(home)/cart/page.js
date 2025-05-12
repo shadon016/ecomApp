ae45297dc6f93b67622a28215d1c715428cdc3d6
shadon016 <shadon016@gmail.com>
2025-05-12 12:53:06 +0600

    update cart components

--- app/(home)/cart/page.js
+++ app/(home)/cart/page.js
@@ -1,9 +1,14 @@
 import CartItem from "./components/cartItem.jsx";
+import Cartimg from "./components/cartimg.jsx";
 const Page = () => {
   return (
-    <div>
-      <p>cart page</p>
-      <CartItem />
+    <div className="flex h-[calc(100vh-10rem)]">
+      <div className="flex-1">
+        <Cartimg />
+      </div>
+      <div className="flex-1">
+        <CartItem />
+      </div>
     </div>
   );
 };
