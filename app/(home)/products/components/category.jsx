c935ceb7bc0f372dd9b748bf6092f1de1ec2e42b
shadon016 <shadon016@gmail.com>
2025-05-12 15:22:56 +0600

    fixing minor change

--- app/(home)/products/components/category.jsx
+++ app/(home)/products/components/category.jsx
@@ -10,10 +10,9 @@ const Category = ({ products }) => {
   const categories = Object.keys(grouped);
   return (
     <div className="sticky top-20 right-0 py-7">
-      <h1>Category</h1>
       {categories?.map((category, index) => (
-        <div key={index}>
-          <p>{category}</p>
+        <div key={index} className="w-full py-3">
+          <p className="py-3 shadow px-6">{category}</p>
         </div>
       ))}
     </div>
